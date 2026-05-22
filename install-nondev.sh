d87fbab9ecce6943eba6261d36191955df5d69f7
Sanjiv Das <srdas@scu.edu>
2026-05-21 23:24:00 -0700

    nondev install script

---
+++ install-nondev.sh
@@ -0,0 +1,19 @@
+git clone https://github.com/srdas/jupyter-ai-quickagent.git
+
+cd jupyter-ai-quickagent
+
+uv venv --python 3.12
+
+uv pip install jupyter-ai
+
+npm install -g @agentclientprotocol/claude-agent-acp
+
+uv pip install jupyter-ai-magic-commands
+
+uv pip install jupyter-ai-jupyternaut
+
+cd ..
+uv pip install -e jupyter-ai-quickagent
+
+cd jupyter-ai-quickagent 
+jupyter lab
