2b34ee3144c88e0bdc7846837966f5838e8761f2
Sanjiv Das <srdas@scu.edu>
2026-05-23 18:04:51 -0700

    Update install_nondev.sh

--- install_nondev.sh
+++ install_nondev.sh
@@ -3,9 +3,8 @@ git clone https://github.com/srdas/jupyter-ai-quickagent.git
 cd jupyter-ai-quickagent
 
 uv venv --python 3.12
-source .venv/bin/activate
-
 sleep 5
+source .venv/bin/activate
 
 uv pip install jupyter-ai
 
